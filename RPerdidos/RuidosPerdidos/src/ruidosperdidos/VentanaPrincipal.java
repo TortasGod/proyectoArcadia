134c1e76d13912c195af833ae334b93d82e7cfae
bapnator <80183828+bapnator@users.noreply.github.com>
2025-06-03 00:07:02 -0700

    inicio sesion y registro
    aun no estan funcionando falta su funcionalidad

--- RPerdidos/RuidosPerdidos/src/ruidosperdidos/VentanaPrincipal.java
+++ RPerdidos/RuidosPerdidos/src/ruidosperdidos/VentanaPrincipal.java
@@ -32,8 +32,8 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
-        jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
@@ -65,13 +65,22 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         jLayeredPane2.add(jLabel4);
         jLabel4.setBounds(300, 70, 30, 16);
 
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+        jLayeredPane2.add(jTextField1);
+        jTextField1.setBounds(100, 20, 190, 30);
+
         jButton1.setText("Registrarse");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         jLayeredPane2.add(jButton1);
         jButton1.setBounds(630, 10, 90, 23);
-
-        jLabel7.setText("ijijdowjofdwojfdowfjwojfwokjflwjflwjfljwl");
-        jLayeredPane2.add(jLabel7);
-        jLabel7.setBounds(110, 30, 170, 16);
 
         jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/barrachida.png"))); // NOI18N
         jLayeredPane2.add(jLabel8);
@@ -86,6 +95,14 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -130,8 +147,8 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLayeredPane jLayeredPane2;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
